c14d7c304d6725c8a2153c9d88a1023bdf4cee3f
Shazzad501 <assazzad501@gmail.com>
2025-01-05 19:14:17 +0600

    get a user if hi is admin

--- index.js
+++ index.js
@@ -46,14 +46,14 @@ async function run() {
 
     // verify jwt token
     const verifyToken = (req, res, next)=>{
-      console.log('Inside verify token',req.headers.authorization)
+      // console.log('Inside verify token',req.headers.authorization)
       if(!req.headers.authorization){
-        return res.status(401).send({message: 'Forbidden Access'})
+        return res.status(401).send({message: 'Unauthorized Access'})
       }
       const token = req.headers.authorization.split(' ')[1];
       jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) =>{
         if(err){
-          return res.status(401).send({message: 'Forbidden Access'})
+          return res.status(401).send({message: 'Unauthorized Access'})
         }
         req.decoded = decoded;
         next()
@@ -86,6 +86,22 @@ async function run() {
       }
       const result = await usersCollection.updateOne(filter, updatedDoc);
       res.send(result);
+    })
+
+    // get a user by check is admin
+    app.get('/users/admin/:email',verifyToken, async(req, res)=>{
+      const email = req.params.email;
+      if(email !== req.decoded.email){
+        return res.status(403).send({message: 'Forbidden Access'})
+      }
+      const query = {email: email};
+      const user = await usersCollection.findOne(query);
+      let admin = false;
+      if(user){
+        admin = user?.role === 'admin';
+      }
+      res.send({admin})
+
     })
 
     // get all user
